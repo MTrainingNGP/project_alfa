66519d7178ce27aa67f50225dbb28449ab0af1d7
utkarsh rai <utkarsh.maximess@gmail.com>
2016-10-25 14:18:22 +0530

    empty validation completed

--- js/registration.js
+++ js/registration.js
@@ -1,14 +1,30 @@
-function disable(){
-	document.getElementById("button_id").disabled=true;
-	alert("hello");
+
+ 
+function empty(element,spanClass){
+	var str= element.value;
+
+	
+	if (str.length==0||str.validity.valueMissing) {
+		element.setCustomValidity("this field cannot be empty");
+		document.getElementById(spanClass).innerHTML+=element.validationMessage+'<br>';
+		console.log(element);
+	}
 }
-function enable(){
-	document.getElementById("button_id").disabled=false;
+
+
+var required= document.querySelectorAll("input[required]");
+var span=document.querySelectorAll("span")
+for (var i = required.length - 1; i >= 0; i--) {
+	(function (){
+		var spanId =required[i].nextSibling.nextSibling.getAttribute('id');
+	var element=required[i];
+	required[i].addEventListener("blur",function (){empty(element,spanId)});
+	})();
 }
-function show(){
-	document.getElementById("disablity_discription_id").style.display="block";
-}
-function hide(){
-	document.getElementById("disablity_discription_value_id").value="";
-	document.getElementById("disablity_discription_id").style.display="none";
+function length(element,spanId){
+	var str= element.value;
+	if (str.length>50||element.validity.tooLong) {
+		element.setCustomValidity("this field cannot be more than 50 characters");
+		document.getElementById(spanId).innerHTML+=element.validationMessage+'<br>';
+	}
 }
--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php require_once("include/header.php"); ?>
 
-  <body onload="disable();hide()">
+  <body >
 
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
@@ -29,41 +29,50 @@
         <div class="form-group row">
   <label for="name" class="col-xs-2 col-form-label">Name</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="fname" placeholder="enter name"  id="fname_id">
+    <input class="form-control" type="text" name="fname" placeholder="enter name"   id="fname_id" maxlength="50" required>
+    <span id="fname_err" class="has_error"> </span>
   </div>
+  
 </div>
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">Last Name</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="lname" placeholder="enter last name"  id="lname_id">
+    <input class="form-control" type="text" name="lname" placeholder="enter last name"  id="lname_id" required maxlength="30">
+    <span id="lname_err" class="has_error"> </span>
   </div>
+
 </div>
 <div class="form-group row">
   <label for="example-email-input" class="col-xs-2 col-form-label">Father Name</label>
   <div class="col-xs-10">
-    <input class="form-control" type="email" name="father_name" placeholder="enter fathers name"  id="father_name_id">
+    <input class="form-control" type="email" name="father_name" placeholder="enter fathers name"  id="father_name_id" required maxlength="100">
+    <span id="father_name_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="mother_name" class="col-xs-2 col-form-label">Mother,s Name</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="mother_name" placeholder="enter mothers name"  id="mother_name_id">
+    <input class="form-control" type="text" name="mother_name" placeholder="enter mothers name"  id="mother_name_id" required maxlength="100">
+    <span id="mother_name_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="email" class="col-xs-2 col-form-label">Email</label>
   <div class="col-xs-10">
-    <input class="form-control" type="email"  id="email_id">
+    <input class="form-control" type="email"  id="email_id" name="email" required maxlength="60">
+    <span id="email_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="date" class="col-xs-2 col-form-label">Date</label>
   <div class="col-xs-10">
-    <input class="form-control" type="date" value="2011-08-19" id="date_id" name="date">
+    <input class="form-control" type="date" value="2011-08-19" id="date_id" name="date" required>
+    <span id="date_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label class="col-xs-2 col-form-label"> Gender</label>
+  <span id="gender_err" class="has_error"> </span>
   <div class="col-xs-6">
     <div class="col-xs-3">
       <label>
@@ -82,69 +91,79 @@
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">Country</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="country"  id="country_id">
+    <input class="form-control" type="text" name="country"  id="country_id" required maxlength="50">
+    <span id="country_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">State</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="state"  id="state_id">
+    <input class="form-control" type="text" name="state"  id="state_id" required maxlength="50">
+    <span id="state_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">City</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="city"  id="city_id">
+    <input class="form-control" type="text" name="city"  id="city_id" required maxlength="50">
+    <span id="city_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">Pin Code</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="pin"  id="pin_id">
+    <input class="form-control" type="text" name="pin"  id="pin_id" maxlength="6">
+    <span id="pin_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">Nationality</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="nationality"  id="nationality_id">
+    <input class="form-control" type="text" name="nationality"  id="nationality_id" required maxlength="20">
+    <span id="nationality_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-search-input" class="col-xs-2 col-form-label">Education</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="education"  id="education_id">
+    <input class="form-control" type="text" name="education"  id="education_id" required maxlength="50">
+    <span id="education_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-tel-input" class="col-xs-2 col-form-label">University</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="university" placeholder="name of university"  id="university_id">
+    <input class="form-control" type="text" name="university" placeholder="name of university"  id="university_id" required maxlength="50">
+    <span id="university_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-tel-input" class="col-xs-2 col-form-label">Telephone</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="phone"  id="phone_id">
+    <input class="form-control" type="text" name="phone"  id="phone_id" maxlength="10">
+    <span id="phone_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-tel-input" class="col-xs-2 col-form-label">Mobile Number</label>
   <div class="col-xs-10">
-    <input class="form-control" type="text" name="mobile"  id="mobile_id">
+    <input class="form-control" type="text" name="mobile"  id="mobile_id" required minlength="10" maxlength="10">
+    <span id="mobile_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label class="col-xs-2 col-form-label"> Physically Disable</label>
+  <span id="diablity_err" class="has_error"> </span>
   <div class="col-xs-6">
     <div class="col-xs-3">
       <label>
-         <input type="radio" onclick="show()" name="phy_disble" id="physically_id" value="yes">
+         <input type="radio"  name="phy_disble" id="physically_id" value="yes">
          Yes
          </label>
     </div>
     <div class="col-xs-3">
       <label>
-        <input type="radio" name="phy_disble"  value="no" onclick="hide()">
+        <input type="radio" name="phy_disble"  value="no" >
          No
   </label>
     </div>
@@ -152,29 +171,33 @@
       <div class="form-group row">
         <label for="example-tel-input" class="col-xs-2 col-form-label">Kind of Disability</label>
        <div class="col-xs-10">
-    <input class="form-control" type="text" id="disablity_discription_value_id" name="disablity_discription"  >
+    <input class="form-control" type="text" id="disablity_discription_value_id" name="disablity_discription" maxlength="20" >
+    <span id="disablity_discription_err" class="has_error"> </span>
   </div>
 </div>
     </div>
   </div>
 </div>
 <div class="form-group row">
-  <label for="password" class="col-xs-2 col-form-label">Password</label>
+  <label for="password" class="col-xs-2 col-form-label" >Password</label>
   <div class="col-xs-10">
-    <input class="form-control" type="password" placeholder="enter password"  name="password" id="password_id">
+    <input class="form-control" type="password" placeholder="enter password"  name="password" id="password_id" required maxlength="10">
+    <span id="password_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <label for="example-password-input" class="col-xs-2 col-form-label">Confirm Password</label>
   <div class="col-xs-10">
-    <input class="form-control" type="password"  placeholder="retype password" id="confirm_pass_id">
+    <input class="form-control" type="password"  placeholder="retype password" id="confirm_password_id" required maxlength="10">
+    <span id="confirm_password_err" class="has_error"> </span>
   </div>
 </div>
 <div class="form-group row">
   <div class="col-xs-10 col-xs-offset-2">
     <label class="checkbox-inline col-xs-1">
-  <input  type="checkbox" id="term_id" value="" onclick="enable()"> </label>
+  <input  type="checkbox" id="term_id" value="" > </label>
   <p class="col-xs-8"><bold>i agree to the terms and condition </bold></p>
+  <span id="term_err" class="has_error"> </span>
 
  
 </div>
